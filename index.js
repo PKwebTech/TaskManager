136dceb8af1a403d2a37bab06b3499067824a22e
PravinKolate <pravinkolate@onnbikes.com>
2023-07-03 21:53:35 +0530

    test

--- index.js
+++ index.js
@@ -39,13 +39,16 @@ app.use(
 jsonfile = __dirname + "/data.json";
 
 //jwt verification
+//jwt verification
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
-  const token = authHeader;
-  console.log(token);
+  const token = authHeader && authHeader.split(" ")[1];
+
   if (token == null) return res.sendStatus(401);
 
   jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    console.log(err);
+
     if (err) return res.sendStatus(403);
 
     req.user = user;
